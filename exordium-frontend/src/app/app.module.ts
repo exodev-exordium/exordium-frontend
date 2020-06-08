ea39c1ee7773a43c6e658656563587381b568f84
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-08 00:10:47 -0400

    update; new  ng components added

--- exordium-frontend/src/app/app.module.ts
+++ exordium-frontend/src/app/app.module.ts
@@ -4,9 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Universal Components
+import { HeaderComponent } from './_components/header/header.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { SidebarComponent } from './_components/sidebar/sidebar.component';
+
+// Pages
+import { WelcomeComponent } from './_pages/welcome/welcome.component';
+import { PricingComponent } from './_pages/pricing/pricing.component';
+import { DownloadComponent } from './_pages/download/download.component';
+import { AboutComponent } from './_pages/company/about/about.component';
+import { ContactComponent } from './_pages/company/contact/contact.component';
+import { LegalComponent } from './_pages/company/legal/legal.component';
+import { OverviewComponent } from './_pages/blog/overview/overview.component';
+import { ArticleComponent } from './_pages/blog/article/article.component';
+import { AuthorComponent } from './_pages/blog/author/author.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+
+    AppComponent,
+    WelcomeComponent,
+    PricingComponent,
+    DownloadComponent,
+    AboutComponent,
+    ContactComponent,
+    LegalComponent,
+    OverviewComponent,
+    ArticleComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
