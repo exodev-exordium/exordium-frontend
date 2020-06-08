d2410af16aba0d36a183276113fbb5a226c5c80c
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-08 02:25:05 -0400

    update; add more routing urls

--- exordium-frontend/src/app/app-routing.module.ts
+++ exordium-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+// Main
 import { WelcomeComponent } from './_pages/welcome/welcome.component';
+import { PricingComponent } from './_pages/pricing/pricing.component';
+import { DownloadComponent } from './_pages/download/download.component';
+
+// Blog
+
+// Company
+import { AboutComponent } from './_pages/company/about/about.component';
+import { ContactComponent } from './_pages/company/contact/contact.component';
 
 
 const routes: Routes = [
@@ -18,6 +28,53 @@ const routes: Routes = [
     data: {
       title: 'Welcome'
     }
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent,
+    data: {
+      title: 'Pricing'
+    }
+  },
+  {
+    path: 'download',
+    component: DownloadComponent,
+    data: {
+      title: 'Download'
+    }
+  },
+  {
+    path: 'blog',
+    component: WelcomeComponent,
+    data: {
+      title: 'Blog'
+    }
+  },
+  {
+    path: 'company',
+    children: [
+      {
+        path: 'about-us',
+        component: AboutComponent,
+        data: {
+          title: 'About'
+        }
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+        data: {
+          title: 'Contact'
+        }
+      },
+      {
+        path: 'legal',
+        component: WelcomeComponent,
+        data: {
+          title: 'Legal Information'
+        }
+      }
+    ]
   }
 ];
 
