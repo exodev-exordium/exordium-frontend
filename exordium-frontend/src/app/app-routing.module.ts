53c696caf7f1b13738d9a8fe7e11165296a1d565
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-08 00:38:34 -0400

    update; routing basics

--- exordium-frontend/src/app/app-routing.module.ts
+++ exordium-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './_pages/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full',
+    data: {
+      title: 'Welcome'
+    }
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
+    data: {
+      title: 'Welcome'
+    }
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      enableTracing: true,
+      scrollPositionRestoration: 'enabled'
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
