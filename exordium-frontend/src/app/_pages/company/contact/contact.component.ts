28ff4dd37c6bbf8887b8e7f769205c30eb291f19
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-08 02:23:05 -0400

    update; contact page

--- exordium-frontend/src/app/_pages/company/contact/contact.component.ts
+++ exordium-frontend/src/app/_pages/company/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { jarallax } from 'jarallax';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      realname: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      company: [null],
+      phone: [null],
+      message: [null, Validators.required],
+      recaptcha: [null, Validators.required]
+    });
+
+    this.jarallaxInit();
+  }
+
+  jarallaxInit() {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.6
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+
   }
 
 }
