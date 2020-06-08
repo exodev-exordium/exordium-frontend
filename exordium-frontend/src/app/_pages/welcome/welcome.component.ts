9419a8e843e4abf9526d4b054f04c44cfeca1fd9
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-08 01:05:58 -0400

    update; welcome page

--- exordium-frontend/src/app/_pages/welcome/welcome.component.ts
+++ exordium-frontend/src/app/_pages/welcome/welcome.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { InlineSVGModule } from 'ng-inline-svg';
+
+import Typed from 'typed.js';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -10,6 +15,21 @@ export class WelcomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.typedjsInit();
+  }
+
+  typedjsInit() {
+    const options = {
+      strings: [
+        'Secure Internet.^2000',
+        'Private Data.^2000',
+        'Military Grade.^2000',
+        'Environmental.^2000',
+      ],
+      loop: true,
+      typeSpeed: 45
+    };
+    const typed = new Typed('.typed', options);
   }
 
 }
