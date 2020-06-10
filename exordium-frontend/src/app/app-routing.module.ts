7f9876858f0889fc168e5b1248204686281d689a
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-10 06:40:17 -0400

    update; remove blog and company components, add new routing modules

--- exordium-frontend/src/app/app-routing.module.ts
+++ exordium-frontend/src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Routing
+import { CompanyModule } from './_pages/company/company.module';
+import { BlogModule } from './_pages/blog/blog.module';
+
 // Main
 import { WelcomeComponent } from './_pages/welcome/welcome.component';
 import { PricingComponent } from './_pages/pricing/pricing.component';
 import { DownloadComponent } from './_pages/download/download.component';
-
-// Blog
-
-// Company
-import { AboutComponent } from './_pages/company/about/about.component';
-import { ContactComponent } from './_pages/company/contact/contact.component';
-import { LegalComponent } from './_pages/company/legal/legal.component';
-import { JobsComponent } from './_pages/company/jobs/jobs.component';
-import { ReviewsComponent } from './_pages/company/reviews/reviews.component';
-
 
 const routes: Routes = [
   {
@@ -45,78 +39,21 @@ const routes: Routes = [
     data: {
       title: 'Download'
     }
-  },
-  {
-    path: 'general',
-    component: WelcomeComponent,
-    data: {
-      title: 'General'
-    }
-  },
-  {
-    path: 'resources',
-    component: WelcomeComponent,
-    data: {
-      title: 'Resources'
-    }
-  },
-  {
-    path: 'blog',
-    component: WelcomeComponent,
-    data: {
-      title: 'Blog'
-    }
-  },
-  {
-    path: 'company',
-    children: [
-      {
-        path: 'about-us',
-        component: AboutComponent,
-        data: {
-          title: 'About'
-        }
-      },
-      {
-        path: 'contact',
-        component: ContactComponent,
-        data: {
-          title: 'Contact'
-        }
-      },
-      {
-        path: 'legal',
-        component: LegalComponent,
-        data: {
-          title: 'Legal Information'
-        }
-      },
-      {
-        path: 'jobs',
-        component: JobsComponent,
-        data: {
-          title: 'Jobs'
-        }
-      },
-      {
-        path: 'reviews',
-        component: ReviewsComponent,
-        data: {
-          title: 'Service Reviews'
-        }
-      }
-    ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(
-    routes,
-    {
-      enableTracing: true,
-      scrollPositionRestoration: 'enabled'
-    }
-  )],
+  imports: [
+    CompanyModule,
+    BlogModule,
+    RouterModule.forRoot(
+      routes,
+      {
+        enableTracing: true,
+        scrollPositionRestoration: 'enabled'
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
