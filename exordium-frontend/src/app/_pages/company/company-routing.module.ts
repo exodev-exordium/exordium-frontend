238c7a993e7483073e15cf0e21290f5f54d13f86
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-10 06:33:43 -0400

    add; company routing module

---
+++ exordium-frontend/src/app/_pages/company/company-routing.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Company
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LegalComponent } from './legal/legal.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+
+
+const routes: Routes = [
+  {
+    path: 'company',
+    children: [
+      {
+        path: 'about-us',
+        component: AboutComponent,
+        data: {
+          title: 'About'
+        }
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+        data: {
+          title: 'Contact'
+        }
+      },
+      {
+        path: 'legal',
+        component: LegalComponent,
+        data: {
+          title: 'Legal Information'
+        }
+      },
+      {
+        path: 'jobs',
+        component: JobsComponent,
+        data: {
+          title: 'Jobs'
+        }
+      },
+      {
+        path: 'reviews',
+        component: ReviewsComponent,
+        data: {
+          title: 'Service Reviews'
+        }
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CompanyRoutingModule { }
