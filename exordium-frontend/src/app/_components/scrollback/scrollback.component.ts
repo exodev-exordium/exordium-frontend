5f89bd7b9b49cdc17c77a6bc064e791bc58a4d01
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-10 21:07:17 -0400

    update; tawk widget added

--- exordium-frontend/src/app/_components/scrollback/scrollback.component.ts
+++ exordium-frontend/src/app/_components/scrollback/scrollback.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-scrollback',
@@ -6,8 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scrollback.component.scss']
 })
 export class ScrollbackComponent implements OnInit {
+  windowScrolled: boolean;
 
-  constructor() { }
+  constructor(
+    @Inject (DOCUMENT) private document: Document
+  ) { }
+
+  @HostListener('window:scroll', [])
+
+  onWindowScroll() {
+    if (window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+    } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = false;
+    }
+  }
+
+  scrollToTop() {
+    (function smoothscroll() {
+      const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - (currentScroll / 8));
+      }
+    })();
+  }
 
   ngOnInit(): void {
   }
