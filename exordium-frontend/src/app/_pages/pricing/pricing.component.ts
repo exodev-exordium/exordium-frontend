dee650108a7821591cd772eb720985d7f8d5d1b8
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-15 16:54:49 -0400

    update; add jarallax

--- exordium-frontend/src/app/_pages/pricing/pricing.component.ts
+++ exordium-frontend/src/app/_pages/pricing/pricing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { jarallax } from 'jarallax';
 
 @Component({
   selector: 'app-pricing',
@@ -10,6 +11,9 @@ export class PricingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.6
+    });
   }
 
 }
