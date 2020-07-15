938a403918736ee80b4bc3d65ca6739a44cc056a
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-15 04:45:12 -0400

    update; basic service update

--- exordium-frontend/src/app/_pages/blog/overview/overview.component.ts
+++ exordium-frontend/src/app/_pages/blog/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from 'src/app/__services/blog.service';
 
 @Component({
   selector: 'app-overview',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
+  // Blog Posts
+  posts: any[];
 
-  constructor() { }
+  constructor(
+    private blogService: BlogService
+  ) { }
 
   ngOnInit(): void {
+    // Get Blog Data
+    this.blogService.getPosts().subscribe(res => {
+      this.posts = res;
+    });
   }
 
 }
