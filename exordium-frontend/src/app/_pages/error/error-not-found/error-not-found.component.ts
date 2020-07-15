df74cfc141839def902ec5feea9ae3eddaa39a29
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-15 04:44:04 -0400

    update; error 404 design

--- exordium-frontend/src/app/_pages/error/error-not-found/error-not-found.component.ts
+++ exordium-frontend/src/app/_pages/error/error-not-found/error-not-found.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { jarallax } from 'jarallax';
+
 @Component({
   selector: 'app-error-not-found',
   templateUrl: './error-not-found.component.html',
@@ -10,6 +12,9 @@ export class ErrorNotFoundComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.3
+    });
   }
 
 }
